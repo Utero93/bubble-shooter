408cfbf8c5e64b5cc5ef8247e7b9fe669aaf291a
Toan <nguyentoan0275@gmail.com>
2024-04-21 15:43:04 -0400

    added redirect to characters page from the scores page with retry button

--- score-page/score.js
+++ score-page/score.js
@@ -1,4 +1,16 @@
-const homeBtn = document.getElementById('returnButton')
+const retryBtn = document.getElementById('retry')
+
+// Variables for end game, input username 
+let endEl = document.getElementById("end");
+let submitEl = document.getElementById("submit");
+let inputEl = document.getElementById("input");
+let finalScoreEl = document.getElementById("finalscore");
+
+// Variables for the scoreboard section
+let scoreSectionEl = document.getElementById("score");
+let scoreboardEl = document.getElementById("scoreboard");
+let retryEl = document.getElementById("retry");
+let clearEl = document.getElementById("clear");
 
 retryBtn.addEventListener('click', function() {
     window.location.href = "../characters-page/characters.html"
@@ -7,3 +19,14 @@ retryBtn.addEventListener('click', function() {
 document.getElementById("retry").addEventListener("click", () => {
     window.location.href = "../characters-page/characters.html"; // Change this URL to the actual game page URL
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // -------======= IT GETS THE ELEMENT WITH THE ID "retry" -------======= \\
+    var returnButton = document.getElementById("retry");
+  
+    // -------======= ADDS AN EVENT LISTENER TO IT FOR THE CLICK EVENT. -------======= \\
+returnButton.addEventListener("click", function () {
+      // -------=======  WHEN THE BUTTON IS CLICKED, IT REDIRECTS THE PAGE TO "characters.html". -------======= \\
+      window.location.href = "../characters-page/characters.html";
+    });
+})
