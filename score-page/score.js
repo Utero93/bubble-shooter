3e4ae2ff828e37e4806f9a9774e61e27b95e5dcc
Utero93 <nirvanautero93@gmail.com>
2024-04-21 15:35:17 -0400

    still fixing path issus for the retry button

--- score-page/score.js
+++ score-page/score.js
@@ -1,29 +1,9 @@
-const retryBtn = document.getElementById("retry");
+const homeBtn = document.getElementById('returnButton')
 
-const scoresEl = document.querySelector("#scoreEl");
-const startGameBtn = document.querySelector("#startGameBtn");
-const modalEl = document.querySelector("#modalEl");
-const bigScoreEl = document.querySelector("#bigScoreEl");
-
-// Variables for end game, input username
-let endEl = document.getElementById("end");
-let submitEl = document.getElementById("submit");
-let inputEl = document.getElementById("input");
-let finalScoreEl = document.getElementById("finalscore");
-
-// Variables for the scoreboard section
-let scoreSectionEl = document.getElementById("score");
-let scoreboardEl = document.getElementById("scoreboard");
-let retryEl = document.getElementById("retry");
-let clearEl = document.getElementById("clear");
-
-retryBtn.addEventListener("click", function () {
-  window.location.href = "../characters-page/characters.html";
-});
+homeBtn.addEventListener('click', function() {
+    window.location.href = "../index.html"
+})
 
 document.getElementById("retry").addEventListener("click", () => {
-  window.location.href = "../characters-page/characters.html"; // Change this URL to the actual game page URL
-});
-
-    
-  
+    window.location.href = "../characters-page/characters.html"; // Change this URL to the actual game page URL
+  });
