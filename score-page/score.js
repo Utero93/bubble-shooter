eff6e053bc0b0e34c3a0f5f27c3757de4fd0e6f1
Toan <nguyentoan0275@gmail.com>
2024-04-21 15:43:04 -0400

    added redirect to characters page from the scores page with retry button

--- score-page/score.js
+++ score-page/score.js
@@ -1,10 +1,5 @@
 const retryBtn = document.getElementById('retry')
 
-const scoresEl = document.querySelector("#scoreEl");
-const startGameBtn = document.querySelector("#startGameBtn");
-const modalEl = document.querySelector("#modalEl");
-const bigScoreEl = document.querySelector("#bigScoreEl");
-
 // Variables for end game, input username 
 let endEl = document.getElementById("end");
 let submitEl = document.getElementById("submit");
@@ -17,6 +12,8 @@ let scoreboardEl = document.getElementById("scoreboard");
 let retryEl = document.getElementById("retry");
 let clearEl = document.getElementById("clear");
 
+retryBtn.addEventListener('click', function() {
+    window.location.href = "../characters-page/characters.html"
 retryBtn.addEventListener('click', function() {
     window.location.href = "../characters-page/characters.html"
 })
