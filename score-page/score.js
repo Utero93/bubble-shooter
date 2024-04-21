018bf948216087090eebdf399783a4aff60c85d3
Utero93 <nirvanautero93@gmail.com>
2024-04-21 15:45:28 -0400

    before pull from develop

--- score-page/score.js
+++ score-page/score.js
@@ -7,3 +7,14 @@ homeBtn.addEventListener('click', function() {
 document.getElementById("retry").addEventListener("click", () => {
     window.location.href = "../characters-page/characters.html"; // Change this URL to the actual game page URL
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // -------======= IT GETS THE ELEMENT WITH THE ID "retry" -------======= \\
+    var returnButton = document.getElementById("retry");
+  
+    // -------======= ADDS AN EVENT LISTENER TO IT FOR THE CLICK EVENT. -------======= \\
+returnButton.addEventListener("click", function () {
+      // -------=======  WHEN THE BUTTON IS CLICKED, IT REDIRECTS THE PAGE TO "characters.html". -------======= \\
+      window.location.href = "../characters-page/characters.html";
+    });
+})
