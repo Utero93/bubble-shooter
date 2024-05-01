3b7c3999423f3f3f53d1565ae5e9504844d89d89
Utero93 <nirvanautero93@gmail.com>
2024-04-30 21:32:16 -0400

    fixing

--- characters-page/character.js
+++ characters-page/character.js
@@ -5,7 +5,6 @@ const deltaShip = document.getElementById("delta");
 const echoShip = document.getElementById("echo");
 const gammaShip = document.getElementById("gamma");
 
-
 const alphaShipBtn = document.getElementById("alpha-ship");
 const betaShipBtn = document.getElementById("beta-ship");
 const charlieShipBtn = document.getElementById("charlie-ship");
@@ -40,25 +39,19 @@ window.onclick = function (event) {
 // create empty vairable for ship, will change based on user choice
 let userShip;
 
+
 // gets the source of the imaage, use url instead? or will run into issues with file locations
-// on click, pass this information to the game and start the game with the user selected avater
 // on click, pass this information to the game and start the game with the user selected avater
 alphaShipBtn.addEventListener("click", function () {
   // gets the path of the image and slices the first period for the right location relative to the index.js
-  userShip = alphaShip.getAttribute("src").slice(1);
   userShip = alphaShip.getAttribute("src").slice(1);
   // stores the variable in the session
   // if user chooses a different ship then it will overwrite the previous ship
   sessionStorage.setItem("userShip", userShip);
   usernameModal.style.display = "block";
-  sessionStorage.setItem("userShip", userShip);
-  usernameModal.style.display = "block";
 });
 
 betaShipBtn.addEventListener("click", function () {
-  userShip = betaShip.getAttribute("src").slice(1);
-  sessionStorage.setItem("userShip", userShip);
-  usernameModal.style.display = "block";
   userShip = betaShip.getAttribute("src").slice(1);
   sessionStorage.setItem("userShip", userShip);
   usernameModal.style.display = "block";
@@ -68,15 +61,9 @@ charlieShipBtn.addEventListener("click", function () {
   userShip = charlieShip.getAttribute("src").slice(1);
   sessionStorage.setItem("userShip", userShip);
   usernameModal.style.display = "block";
-  userShip = charlieShip.getAttribute("src").slice(1);
-  sessionStorage.setItem("userShip", userShip);
-  usernameModal.style.display = "block";
 });
 
 deltaShipBtn.addEventListener("click", function () {
-  userShip = deltaShip.getAttribute("src").slice(1);
-  sessionStorage.setItem("userShip", userShip);
-  usernameModal.style.display = "block";
   userShip = deltaShip.getAttribute("src").slice(1);
   sessionStorage.setItem("userShip", userShip);
   usernameModal.style.display = "block";
